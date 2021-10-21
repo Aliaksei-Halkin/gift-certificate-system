cc62e4d6deff694b8994d89145864d147a20b973
Aliaksei Halkin <pochtax3@gmail.com>
2021-10-21 12:27:39 +0300

    fixed adding tags

--- service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
@@ -48,6 +48,12 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         this.tagValidator = tagValidator;
     }
 
+    /**
+     * The method represents adding a new {@link GiftCertificate }to the database
+     *
+     * @param giftCertificate new {@link GiftCertificate }
+     * @return updated and added {@link GiftCertificate }
+     */
     @Override
     @Transactional
     public GiftCertificate addGiftCertificate(GiftCertificate giftCertificate) {
@@ -67,7 +73,7 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
     }
 
     /**
-     * Initially, we check if the tag exists in the database. If it is not, we add it to the certificate.
+     * Initially, we check if the tag exists in the database. If it is not, we add it to the database.
      * If the tag exists in the database, but is not added to the certificate, we add it to the certificate
      *
      * @param giftCertificateId {@code Long} unique identifier of gift certificate
@@ -75,19 +81,33 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
      */
     private void attachedTag(Long giftCertificateId, Tag tag) {
         boolean isPresentTag = tagDao.findTagByName(tag.getName()).isPresent();
-        if (!isPresentTag) {
-            long tagId = tagDao.add(tag);
-            giftCertificateDao.attachedTag(tagId, giftCertificateId);
+        if (isPresentTag == false) {
+            tagDao.add(tag);
         }
-        if (!giftCertificateDao.findGiftCertificateTags(giftCertificateId)//todo don't work
-                .stream()
-                .map(t -> t.getName())
-                .anyMatch(x -> x.equals(tag.getName()))) {
-            giftCertificateDao.attachedTag(tag.getId(), giftCertificateId);
+        Tag movableTag = tagDao.findTagByName(tag.getName()).get();
+        if (isPresentTagInGiftCertificate(giftCertificateId, movableTag) == false) {
+            giftCertificateDao.attachedTag(movableTag.getId(), giftCertificateId);
         }
     }
 
+    /**
+     * Check is present tag in gift certificate
+     *
+     * @param giftCertificateId id of gift certificate
+     * @param tag               {@link Tag}
+     * @return true-if present or else
+     */
+    private boolean isPresentTagInGiftCertificate(Long giftCertificateId, Tag tag) {
+        return giftCertificateDao.findGiftCertificateTags(giftCertificateId).contains(tag);
+    }
 
+    /**
+     * The method represents adding the {@link Tag} to the {@link GiftCertificate }
+     *
+     * @param giftCertificateId id of gift certificate
+     * @param tag               {@link Tag}
+     * @return {@link GiftCertificate } with all tags
+     */
     @Override
     @Transactional
     public GiftCertificate addTagToGiftCertificate(Long giftCertificateId, Tag tag) {
@@ -103,12 +123,24 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         return updatedGiftCertificate;
     }
 
+    /**
+     * The method represents getting the gift certificate from the database
+     *
+     * @param id unique id of gift certificate
+     * @return iftCertificate
+     */
     private GiftCertificate checkAndGetGiftCertificate(Long id) {
         Optional<GiftCertificate> giftCertificateOptional = giftCertificateDao.findById(id);
         return giftCertificateOptional
                 .orElseThrow(() -> new ResourceNotFoundException(ExceptionPropertyKey.GIFT_CERTIFICATE_WITH_ID_NOT_FOUND, id));
     }
 
+    /**
+     * The method represents finding a gift certificate
+     *
+     * @param id unique id of gift certificate
+     * @return GiftCertificate
+     */
     @Override
     public GiftCertificate findGiftCertificateById(Long id) {
         giftCertificateValidator.isValidId(id);
@@ -118,6 +150,12 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         return giftCertificate;
     }
 
+    /**
+     * The method represents finding a gift certificate by query parameters
+     *
+     * @param queryParameter query parameters
+     * @return GiftCertificates
+     */
     @Override
     public List<GiftCertificate> findGiftCertificatesByParameters(QueryParameter queryParameter) {
         QueryParameterValidator.isValidQueryParameters(queryParameter);
@@ -131,12 +169,24 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         return giftCertificates;
     }
 
+    /**
+     * The method represents delete GiftCertificate by Id
+     *
+     * @param id id unique id of gift certificate
+     */
     @Override
     public void deleteGiftCertificateById(Long id) {
         giftCertificateValidator.isValidId(id);
         giftCertificateDao.removeById(id);
     }
 
+    /**
+     * The method represents updating
+     *
+     * @param giftCertificateId the unique id of the gift certificate
+     * @param giftCertificate   giftCertificate with new data
+     * @return updatedGiftCertificate
+     */
     @Override
     public GiftCertificate updateGiftCertificate(Long giftCertificateId, GiftCertificate giftCertificate) {
         giftCertificateValidator.isValidId(giftCertificateId);
@@ -151,6 +201,12 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         return updatedGiftCertificate;
     }
 
+    /**
+     * Update fields
+     *
+     * @param receivedGiftCertificate certificate with new fields
+     * @param updatedGiftCertificate  certificate from database with old fields
+     */
     private void updateFields(GiftCertificate receivedGiftCertificate, GiftCertificate updatedGiftCertificate) {
         if (receivedGiftCertificate.getName() != null && !receivedGiftCertificate.getName().isEmpty()) {
             updatedGiftCertificate.setName(receivedGiftCertificate.getName());
