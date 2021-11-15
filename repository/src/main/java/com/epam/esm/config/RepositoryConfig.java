269876220b4d08b93615a63cd96bd7e1f2cb8bbc
Aliaksei Halkin <pochtax3@gmail.com>
2021-11-15 12:22:30 +0300

    some changes

--- repository/src/main/java/com/epam/esm/config/RepositoryConfig.java
+++ repository/src/main/java/com/epam/esm/config/RepositoryConfig.java
@@ -27,79 +27,79 @@ import java.beans.PropertyVetoException;
  *
  * @author Aliaksei Halkin
  */
-@Configuration
-@ComponentScan("com.epam.esm")
+//@Configuration
+//@ComponentScan("com.epam.esm")
 public class RepositoryConfig {
-    private static final String PACKAGE_TO_SCAN = "com.epam.esm.entity";
-    /**
-     * The first configuration- a dev, for development
-     */
-    @Bean
-    @Profile("dev")
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource developmentDataSource() {
-        return DataSourceBuilder.create().type(DriverManagerDataSource.class).build();
-    }
-
-    /**
-     * The second configuration- a prod, for production.We use c3p0  for create pool connection
-     *
-     * @param driverName driver for connection
-     * @param url        url for connection
-     * @param username   username for connection
-     * @param password   password for connection
-     * @return {@link DataSource} A factory for connections to the physical data source that this DataSource object represents.
-     */
-    @Bean
-    @Profile("prod")
-    @ConfigurationProperties("spring.c3p0")
-    public DataSource productionDataSource(@Value("${spring.c3p0.driverClass}") String driverName,
-                                           @Value("${spring.c3p0.jdbcUrl}") String url,
-                                           @Value("${spring.c3p0.username}") String username,
-                                           @Value("${spring.c3p0.password}") String password,
-                                           @Value("${spring.c3p0.initial_pool_size}") int initialPoolSize,
-                                           @Value("${spring.c3p0.max_pool_size}") int maxPoolSize) {
-        ComboPooledDataSource dataSource = new ComboPooledDataSource();
-        try {
-            dataSource.setDriverClass(driverName);
-            dataSource.setJdbcUrl(url);
-            dataSource.setUser(username);
-            dataSource.setPassword(password);
-            dataSource.setInitialPoolSize(initialPoolSize);
-            dataSource.setMaxPoolSize(maxPoolSize);
-            return dataSource;
-        } catch (PropertyVetoException e) {
-            throw new CannotGetJdbcConnectionException("Error while get connection to database");
-        }
-    }
-
-    @Profile("test")
-    @Bean
-    public DataSource testDataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:schema.sql").addScript("classpath:test-data.sql").setScriptEncoding("UTF-8").build();
-    }
-
-    @Profile(value = {"dev", "prod"})
-    @Bean
-    public LocalContainerEntityManagerFactoryBean managerFactory(DataSource dataSource,
-                                                                 EntityManagerFactoryBuilder entityManagerFactoryBuilder) {
-        return entityManagerFactoryBuilder.dataSource(dataSource).packages(PACKAGE_TO_SCAN).build();
-    }
-
-    @Profile("test")
-    @Bean
-    public LocalContainerEntityManagerFactoryBean testManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(dataSource);
-        entityManager.setPackagesToScan(PACKAGE_TO_SCAN);
-        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        return entityManager;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        return new JpaTransactionManager(entityManagerFactory);
-    }
+//    private static final String PACKAGE_TO_SCAN = "com.epam.esm.entity";
+//    /**
+//     * The first configuration- a dev, for development
+//     */
+//    @Bean
+//    @Profile("dev")
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource developmentDataSource() {
+//        return DataSourceBuilder.create().type(DriverManagerDataSource.class).build();
+//    }
+//
+//    /**
+//     * The second configuration- a prod, for production.We use c3p0  for create pool connection
+//     *
+//     * @param driverName driver for connection
+//     * @param url        url for connection
+//     * @param username   username for connection
+//     * @param password   password for connection
+//     * @return {@link DataSource} A factory for connections to the physical data source that this DataSource object represents.
+//     */
+//    @Bean
+//    @Profile("prod")
+//    @ConfigurationProperties("spring.c3p0")
+//    public DataSource productionDataSource(@Value("${spring.c3p0.driverClass}") String driverName,
+//                                           @Value("${spring.c3p0.jdbcUrl}") String url,
+//                                           @Value("${spring.c3p0.username}") String username,
+//                                           @Value("${spring.c3p0.password}") String password,
+//                                           @Value("${spring.c3p0.initial_pool_size}") int initialPoolSize,
+//                                           @Value("${spring.c3p0.max_pool_size}") int maxPoolSize) {
+//        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+//        try {
+//            dataSource.setDriverClass(driverName);
+//            dataSource.setJdbcUrl(url);
+//            dataSource.setUser(username);
+//            dataSource.setPassword(password);
+//            dataSource.setInitialPoolSize(initialPoolSize);
+//            dataSource.setMaxPoolSize(maxPoolSize);
+//            return dataSource;
+//        } catch (PropertyVetoException e) {
+//            throw new CannotGetJdbcConnectionException("Error while get connection to database");
+//        }
+//    }
+//
+//    @Profile("test")
+//    @Bean
+//    public DataSource testDataSource() {
+//        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+//                .addScript("classpath:schema.sql").addScript("classpath:test-data.sql").setScriptEncoding("UTF-8").build();
+//    }
+//
+//    @Profile(value = {"dev", "prod"})
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean managerFactory(DataSource dataSource,
+//                                                                 EntityManagerFactoryBuilder entityManagerFactoryBuilder) {
+//        return entityManagerFactoryBuilder.dataSource(dataSource).packages(PACKAGE_TO_SCAN).build();
+//    }
+//
+//    @Profile("test")
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean testManagerFactory(DataSource dataSource) {
+//        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+//        entityManager.setDataSource(dataSource);
+//        entityManager.setPackagesToScan(PACKAGE_TO_SCAN);
+//        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+//        return entityManager;
+//    }
+//
+//    @Bean
+//    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+//        return new JpaTransactionManager(entityManagerFactory);
+//    }
 
 }
