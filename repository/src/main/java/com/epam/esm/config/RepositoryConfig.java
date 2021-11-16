ce6f2bebdb085bb5bb6d2ce392c0df99003ed021
Aliaksei Halkin <pochtax3@gmail.com>
2021-11-16 16:33:30 +0300

    some changes

--- repository/src/main/java/com/epam/esm/config/RepositoryConfig.java
+++ repository/src/main/java/com/epam/esm/config/RepositoryConfig.java
@@ -1,14 +1,12 @@
 package com.epam.esm.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import jdk.nashorn.internal.objects.annotations.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -31,6 +29,7 @@ import java.beans.PropertyVetoException;
 @ComponentScan("com.epam.esm")
 public class RepositoryConfig {
     private static final String PACKAGE_TO_SCAN = "com.epam.esm.entity";
+
     /**
      * The first configuration- a dev, for development
      */
@@ -38,7 +37,17 @@ public class RepositoryConfig {
     @Profile("dev")
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource developmentDataSource() {
-        return DataSourceBuilder.create().type(DriverManagerDataSource.class).build();
+      //  return DataSourceBuilder.create().type(DriverManagerDataSource.class).build();
+        //  todo: don't connect to  @ConfigurationProperties(prefix = "spring.datasource")
+
+        DataSource dataSource = DataSourceBuilder
+                .create()
+                .username("root")
+                .password("root")
+                .url("jdbc:mysql://localhost:3306/certificatesdb?useUnicode=true&serverTimezone=UTC")
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .build();
+        return dataSource;
     }
 
     /**
@@ -73,12 +82,12 @@ public class RepositoryConfig {
         }
     }
 
-    @Profile("test")
-    @Bean
-    public DataSource testDataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:schema.sql").addScript("classpath:test-data.sql").setScriptEncoding("UTF-8").build();
-    }
+//    @Profile("test")
+//    @Bean
+//    public DataSource testDataSource() {
+//        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+//                .addScript("classpath:schema.sql").addScript("classpath:test-data.sql").setScriptEncoding("UTF-8").build();
+//    }
 
     @Profile(value = {"dev", "prod"})
     @Bean
@@ -87,15 +96,15 @@ public class RepositoryConfig {
         return entityManagerFactoryBuilder.dataSource(dataSource).packages(PACKAGE_TO_SCAN).build();
     }
 
-    @Profile("test")
-    @Bean
-    public LocalContainerEntityManagerFactoryBean testManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
-        entityManager.setDataSource(dataSource);
-        entityManager.setPackagesToScan(PACKAGE_TO_SCAN);
-        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        return entityManager;
-    }
+//    @Profile("test")
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean testManagerFactory(DataSource dataSource) {
+//        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+//        entityManager.setDataSource(dataSource);
+//        entityManager.setPackagesToScan(PACKAGE_TO_SCAN);
+//        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+//        return entityManager;
+//    }
 
     @Bean
     public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
