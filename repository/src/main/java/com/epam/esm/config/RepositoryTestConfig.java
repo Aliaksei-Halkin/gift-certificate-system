26cec5e80ce440ee84a65f142931eecdbefefed4
Aliaksei Halkin <pochtax3@gmail.com>
2021-11-19 21:42:04 +0300

    Refactored a config file

---
+++ repository/src/main/java/com/epam/esm/config/RepositoryTestConfig.java
@@ -0,0 +1,49 @@
+package com.epam.esm.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * The configuration for integration test repository layer
+ *
+ * @author Aliaksei Halkin
+ */
+@Configuration
+@ComponentScan("com.epam.esm")
+public class RepositoryTestConfig {
+    private static final String PACKAGE_TO_SCAN = "com.epam.esm.entity";
+
+    @Profile("test")
+    @Bean
+    public DataSource testDataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql").addScript("classpath:test-data.sql").setScriptEncoding("UTF-8").build();
+    }
+
+    @Profile("test")
+    @Bean
+    public LocalContainerEntityManagerFactoryBean testManagerFactory(@Qualifier("testDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource);
+        entityManager.setPackagesToScan(PACKAGE_TO_SCAN);
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return entityManager;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
