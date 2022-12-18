0f0e7360638505ae074facfcec40522485147528
Aliaksei Halkin <pochtax3@gmail.com>
2022-12-18 12:53:23 +0300

    added a security config

--- web/src/main/java/com/epam/esm/config/SecurityConfig.java
+++ web/src/main/java/com/epam/esm/config/SecurityConfig.java
@@ -1,11 +1,61 @@
 package com.epam.esm.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
+import com.epam.esm.filter.JwtTokenFilter;
+import com.epam.esm.filter.UserAccessFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtTokenFilter jwtTokenFilter;
+    private final UserAccessFilter userAccessFilter;
 
-public class SecurityConfig {
+    @Autowired
+    public SecurityConfig(JwtTokenFilter jwtTokenFilter, UserAccessFilter userAccessFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+        this.userAccessFilter = userAccessFilter;
+    }
 
+    public SecurityConfig(boolean disableDefaults, JwtTokenFilter jwtTokenFilter, UserAccessFilter userAccessFilter) {
+        super(disableDefaults);
+        this.jwtTokenFilter = jwtTokenFilter;
+        this.userAccessFilter = userAccessFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/actuator/*").hasAuthority("actuator:action")
+                .and()
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(userAccessFilter, JwtTokenFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 
 }
